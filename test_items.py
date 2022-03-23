216a3c34798bf7c7f8844f9c17f0ac8c7aa7ad02
Арсений Анохин <a.anokhin@IAT>
2022-03-23 12:58:25 +0300

    fix name

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_basket_button(browser):
     browser.implicitly_wait(10)
     browser.get(link)
     browser.implicitly_wait(10)
